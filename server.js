df29a9fbb76abf899cd882e5ea1d786846aa3e74
maimu1113-coder <m.aimu1113@icloud.com>
2026-01-13 14:17:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-let tiktokConnection; // 接続を1つに固定
+let tiktokConnection;
 
 io.on('connection', (socket) => {
     socket.on('setTargetUser', (uniqueId) => {
-        // 前の接続が残っていたら完全に停止させる
         if (tiktokConnection) {
             tiktokConnection.disconnect();
             tiktokConnection = null;
@@ -22,20 +21,23 @@ io.on('connection', (socket) => {
         tiktokConnection = new WebcastPushConnection(uniqueId);
 
         tiktokConnection.connect().then(state => {
-            // 接続成功時にデータを送信
+            // アイコンURLが深い階層にある場合も考慮
+            const owner = state.roomInfo.owner;
+            const avatarUrl = owner.avatar_large?.url_list[0] || owner.avatar_medium?.url_list[0] || owner.avatar_thumb?.url_list[0];
+            
             socket.emit('roomInfo', {
-                nickname: state.roomInfo.owner.nickname,
-                avatar: state.roomInfo.owner.avatar_thumb.url_list[0],
-                followerCount: state.roomInfo.owner.stats.follower_count,
+                nickname: owner.nickname || uniqueId,
+                avatar: avatarUrl,
+                followerCount: owner.stats?.follower_count || 0,
                 viewerCount: state.viewerCount || 0
             });
         }).catch(err => console.error('Connect Error', err));
 
-        // イベントリスナーも1回だけ登録されるように socket ではなく個別に管理
         tiktokConnection.on('roomUser', data => {
             socket.emit('viewerUpdate', { viewerCount: data.viewerCount });
         });
 
+        // 二重送信を防ぐため、1つのコネクションにつき1回だけemit
         tiktokConnection.on('chat', data => {
             socket.emit('chat', { nickname: data.nickname, comment: data.comment, msgId: data.msgId });
         });
@@ -51,4 +53,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Server Ready`));
+server.listen(PORT, () => console.log(`System Online`));
