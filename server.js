c4eda5b9c268d5f709a23307551eec5ae8310d92
maimu1113-coder <m.aimu1113@icloud.com>
2026-01-13 15:37:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,47 +13,28 @@ let tiktokConnection;
 
 io.on('connection', (socket) => {
     socket.on('setTargetUser', (uniqueId) => {
-        if (tiktokConnection) {
-            tiktokConnection.disconnect();
-            tiktokConnection = null;
-        }
-
+        if (tiktokConnection) { tiktokConnection.disconnect(); }
         tiktokConnection = new WebcastPushConnection(uniqueId);
 
         tiktokConnection.connect().then(state => {
-            const room = state.roomInfo;
-            const owner = room.owner;
-            
-            // アイコンURLの取得を強化
-            const avatarUrl = owner.avatar_large?.url_list[0] || owner.avatar_thumb?.url_list[0] || "";
-            // フォロワー数の取得を多重化
-            const followers = owner.stats?.follower_count || room.anchor_show_info?.follower_count || 0;
-
+            const owner = state.roomInfo.owner;
             socket.emit('roomInfo', {
                 nickname: owner.nickname || uniqueId,
-                avatar: avatarUrl,
-                followerCount: followers,
+                avatar: owner.avatar_thumb?.url_list[0] || "",
+                followerCount: owner.stats?.follower_count || 0,
                 viewerCount: state.viewerCount || 0
             });
-        }).catch(err => console.error('TikTok Connect Error:', err));
-
-        tiktokConnection.on('roomUser', data => {
-            socket.emit('viewerUpdate', { viewerCount: data.viewerCount });
-        });
+        }).catch(err => console.error('TikTok Error:', err));
 
         tiktokConnection.on('chat', data => {
             socket.emit('chat', { nickname: data.nickname, comment: data.comment, msgId: data.msgId });
         });
-
-        tiktokConnection.on('gift', data => {
-            socket.emit('gift', { nickname: data.nickname, giftName: data.giftName });
+        
+        tiktokConnection.on('roomUser', data => {
+            socket.emit('viewerUpdate', { viewerCount: data.viewerCount });
         });
-    });
-
-    socket.on('disconnect', () => {
-        if (tiktokConnection) tiktokConnection.disconnect();
     });
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Mac Talk System Online`));
+server.listen(PORT, () => console.log(`Server running`));
